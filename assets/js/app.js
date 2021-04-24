ee1344132098de416df27757ad62efb738ba6f0e
Ralph Samaniego <rapsamaniego27@gmail.com>
2021-04-24 17:26:51 +0800

    added first data category to auto dsplay

--- assets/js/app.js
+++ assets/js/app.js
@@ -290,12 +290,14 @@ class UI {
 window.addEventListener('DOMContentLoaded', () => {
   //const scroll = new Scroll();
   const portfolioContainer = document.querySelector('.portfolio__container');
+  const projectTypeOptions = document.querySelectorAll('.project__option');
 
   if (isInPage(portfolioContainer)) {
 
     const portfolio = new Portfolio();
+    const firstProjectCategory = projectTypeOptions[0].dataset.projectType;
 
-    portfolio.displayResults('website-design');
+    portfolio.displayResults(firstProjectCategory);
   }
 
 
